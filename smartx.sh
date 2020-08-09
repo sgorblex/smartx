2a56ce0becc8a90924f91f87a1a73c1554dee9b8
Alessandro Clerici <ale.clericil@gmail.com>
2020-08-09 03:15:08 +0200

    fixed dialog, switched to one file only

--- smartx.sh
+++ smartx.sh
@@ -2,10 +2,31 @@
 
 # TODO
 # fix argument and return values as echos instead of using global variables
+# if flag options will be added, create an option for the part of the script which will be executed by startx (e.g. --startx <commands>)
 # support multiple word names(?)
 # ...
 
-# add your WM's starting command in the list if it doesn't appear. If there's more then one, separate them with ; or &&. The last command must be the one which actually starts the session and should start with exec.
+
+
+# if there's an argument, the script has been called by startx (see below)
+
+if [[ -n $@ ]]
+then
+	# source user's xinitrc if present
+	if [[ -f ~/.xinitrc ]]
+	then
+		source ~/.xinitrc
+	fi
+
+
+	# start the session chosen by smartx
+	eval $@
+
+	exit
+fi
+
+
+# add your WM's starting command in the list if it doesn't appear (don't leave it empty). If there's more then one, separate them with ; or &&. The last command must be the one which actually starts the session and should start with exec.
 declare -A possible_sessions
 possible_sessions[i3]="exec i3"
 possible_sessions[Openbox]="exec openbox-session"
@@ -54,11 +75,11 @@ function menu {
 		if $(command -v ${exec_cmd% *} &> /dev/null)
 		then
 			menu_entries+=($session)
-			menu_entries+=('NULL')
+			[[ $tui == whiptail ]] && menu_entries+=('NULL')
 		fi
 	done
 
-	local chosen_session=$(whiptail --title "smartx" --noitem --menu "Choose a window manager / desktop environment to start" 18 60 10 ${menu_entries[@]} 3>&1 1>&2 2>&3)
+	local chosen_session=$($tui --title "smartx" --noitem --menu "Choose a window manager / desktop environment to start" 18 60 10 ${menu_entries[@]} 3>&1 1>&2 2>&3)
 	
 	echo $chosen_session
 }
@@ -92,5 +113,4 @@ then
 	exit
 fi
 
-script_path=$(realpath $0)
-startx ${script_path%/*}/smartxrc ${possible_sessions[$chosen_session]}
+startx $0 ${possible_sessions[$chosen_session]}
